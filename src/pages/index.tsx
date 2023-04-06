fe31c6d34d518dbb682121d5e213da72d8ee192c
Th3R1c0 <ricots114@protonmail.com>
2023-04-05 18:08:31 -0700

    prisma update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,9 @@ import { SignInButton, SignOutButton, SignUp, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -18,6 +19,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         {!user.isSignedIn && <SignOutButton />}
         {!!user.isSignedIn && <SignInButton />}
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
